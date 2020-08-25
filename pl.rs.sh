a6cdd218615910a91fd6fc322cf24cfa55c72955
shosatojp <me@shosato.jp>
2020-08-25 14:20:59 +0900

    prompt command

--- pl.rs.sh
+++ pl.rs.sh
@@ -1,1 +1,3 @@
-./target/debug/pl --home $HOME --pwd $PWD --error $? --width `tput cols`
+function create_ps1() {
+    PS1=$($(dirname $BASH_SOURCE)/target/debug/pl --home $HOME --pwd $PWD --error $? --width `tput cols`)
+}
