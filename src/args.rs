854f68294aae765ac419b2f44188b8814ccf73a7
shosatojp <me@shosato.jp>
2020-09-01 00:59:43 +0900

    version

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,6 @@
 pub fn get_arg_matches<'a>() -> clap::ArgMatches<'a> {
     return clap::App::new("powerline-shell")
-        .version("0.1.0")
+        .version("0.0.3")
         .arg(
             clap::Arg::with_name("home")
                 .help("specify home directory")
